1195f78bd584bf71f8bff38074ced89180432d43
Pranav-Patani <pranav.patani@gmail.com>
2024-01-29 22:38:38 +0530

    feat: added carousel (img as bkg)

--- newportal/src/App.jsx
+++ newportal/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./assets/components/Nav";
-
+import NewsCarousel from "./assets/components/NewsCarousel";
+import "./assets/css/carousel.css";
 function App() {
   return (
     <>
       <NavBar />
+      <div className="news-carousel">
+        <NewsCarousel />
+      </div>
     </>
   );
 }
